b7a8e895f0f7d86373b188d4445e236dd018d9e1
Andrew Farquharson <andrewfarquharson@So-Much-For-Subtlety.local>
2023-08-14 12:51:30 +1200

    add missing cases

--- Sources/SwiftyTube/Models/InitialPlayer/CaptionKind.swift
+++ Sources/SwiftyTube/Models/InitialPlayer/CaptionKind.swift
@@ -8,7 +8,17 @@
 import Foundation
 
 public enum CaptionKind: String {
+    // A caption track generated using automatic speech recognition.
     case asr
+
+    // A caption track that plays when no other track is selected in the player. 
+    // For example, a video that shows aliens speaking in an alien language might 
+    // have a forced caption track to only show subtitles for the alien language.
+    case forced
+
+    // A regular caption track. This is the default value.
+    case standard
+
     case unknown
 }
 
