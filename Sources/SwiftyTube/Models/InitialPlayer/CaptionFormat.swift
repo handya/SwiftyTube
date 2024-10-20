5e4c380a530936c506fc85c8eb16efdeec6f186a
Andrew Farquharson <andrewfarquharson@Now-We-Try-It-My-Way.local>
2024-10-21 09:01:52 +1300

    feat: improve caption handling

---
+++ Sources/SwiftyTube/Models/InitialPlayer/CaptionFormat.swift
@@ -0,0 +1,42 @@
+//
+//  CaptionFormat.swift
+//  SwiftyTube
+//
+//  Created by Andrew Farquharson on 21/10/2024.
+//
+
+import Foundation
+
+public enum CaptionFormat: String {
+    case vtt
+    case ttml
+    case srv3
+    case srv2
+    case srv1
+    case json3
+}
+
+// MARK: - Codable
+
+extension CaptionFormat: Codable { }
+
+// MARK: - Computed
+
+public extension CaptionFormat {
+    var name: String {
+        switch self {
+        case .vtt:
+            return "WebVTT"
+        case .ttml:
+            return "Timed Text Markup Language"
+        case .srv3:
+            return "Structured Real-time Voice 3"
+        case .srv2:
+            return "Structured Real-time Voice 2"
+        case .srv1:
+            return "Structured Real-time Voice `"
+        case .json3:
+            return "JSON"
+        }
+    }
+}
